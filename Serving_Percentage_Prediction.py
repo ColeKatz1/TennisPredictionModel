a5d9ae2646a37e8481ac3df6b8262e2302b762e3
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-04-11 10:56:58 -0700

    Update Serving_Percentage_Prediction.py
    Added comments

--- Serving_Percentage_Prediction.py
+++ Serving_Percentage_Prediction.py
@@ -6,80 +6,79 @@ from bs4 import BeautifulSoup, Comment
 import pandas as pd
 from selenium import webdriver
 
-with open("C:/Users/colek/OneDrive/Desktop/ATP_Serve_Return_Stats/ATP_Serve_4_7_22.html") as fp:
+with open("C:/Users/colek/OneDrive/Desktop/ATP_Serve_Return_Stats/ATP_Serve_4_7_22.html") as fp: #open html file of serving data
     soup = BeautifulSoup(fp, 'html.parser')
 
-tr = soup.findAll('tr', class_ = "stats-listing-row")
+tr = soup.findAll('tr', class_ = "stats-listing-row") #find table of stats
 
-game_data = [[td.getText() for td in tr[i].findAll(['th','td'])]
+game_data = [[td.getText() for td in tr[i].findAll(['th','td'])] #retrieves the stats from each column in table
         for i in range(len(tr))
         ]
 
-data = pd.DataFrame(game_data)
-data = data.drop(0,axis=1)
-data.columns = ['Player', 'Serve_Rating','1st_Serve_Pct', '1st_Serve_Won_Pct','2nd_Serve_Won_Pct','Serve_Games_Won_Pct','Avg_Aces','Avg_DF']
-#print(data)
-firstServePct = data['1st_Serve_Pct'].astype(str)
+data = pd.DataFrame(game_data) #transforms data into dataframe
+data = data.drop(0,axis=1) #drops unneccesary column
+data.columns = ['Player', 'Serve_Rating','1st_Serve_Pct', '1st_Serve_Won_Pct','2nd_Serve_Won_Pct','Serve_Games_Won_Pct','Avg_Aces','Avg_DF'] #creates header for columns in dataframe
+firstServePct = data['1st_Serve_Pct'].astype(str) 
 firstServePct = firstServePct.tolist()
-firstServePct = [i.replace('%','') for i in firstServePct]
+firstServePct = [i.replace('%','') for i in firstServePct] #removes % from the data values in the column
 
 firstServeWonPct = data['1st_Serve_Won_Pct'].astype(str)
 firstServeWonPct = firstServeWonPct.tolist()
-firstServeWonPct = [i.replace('%','') for i in firstServeWonPct]
+firstServeWonPct = [i.replace('%','') for i in firstServeWonPct] #removes % from the data values in the column
 
 secondServeWonPct = data['2nd_Serve_Won_Pct'].astype(str)
 secondServeWonPct = secondServeWonPct.tolist()
-secondServeWonPct = [i.replace('%','') for i in secondServeWonPct]
+secondServeWonPct = [i.replace('%','') for i in secondServeWonPct] #removes % from the data values in the column
 
 
 
 #secondServePct = [100 - i for i in firstServePctInt]
 
 
-data['1st_Serve_Pct'] = firstServePct
-data['1st_Serve_Won_Pct'] = firstServeWonPct
-data['2nd_Serve_Won_Pct'] = secondServeWonPct
+data['1st_Serve_Pct'] = firstServePct #adds back data without % so the numbers can be used
+data['1st_Serve_Won_Pct'] = firstServeWonPct #adds back data without % so the numbers can be used
+data['2nd_Serve_Won_Pct'] = secondServeWonPct #adds back data without % so the numbers can be used
 
-data['1st_Serve_Pct'] = pd.to_numeric(data['1st_Serve_Pct'])
-data['1st_Serve_Won_Pct'] = pd.to_numeric(data['1st_Serve_Won_Pct'])
-data['2nd_Serve_Won_Pct'] = pd.to_numeric(data['2nd_Serve_Won_Pct'])
-data['2nd_Serve_Pct'] = 100 - data['1st_Serve_Pct']
-data['Pct_Won_On_Serve'] = (data['1st_Serve_Pct'] / 100 * data['1st_Serve_Won_Pct']) + (data['2nd_Serve_Pct'] / 100 * data['2nd_Serve_Won_Pct'])
+data['1st_Serve_Pct'] = pd.to_numeric(data['1st_Serve_Pct']) #converts to numeric
+data['1st_Serve_Won_Pct'] = pd.to_numeric(data['1st_Serve_Won_Pct']) #converts to numeric
+data['2nd_Serve_Won_Pct'] = pd.to_numeric(data['2nd_Serve_Won_Pct']) #converts to numeric
+data['2nd_Serve_Pct'] = 100 - data['1st_Serve_Pct'] #creates variable for percent of time second served is used, which is inversely proportional to times when first serve is successful
+data['Pct_Won_On_Serve'] = (data['1st_Serve_Pct'] / 100 * data['1st_Serve_Won_Pct']) + (data['2nd_Serve_Pct'] / 100 * data['2nd_Serve_Won_Pct']) #gives percent of points won on a player's serve
 
 
-with open("C:/Users/colek/OneDrive/Desktop/ATP_Serve_Return_Stats/ATP_Return_4_7_22.html") as fp:
+with open("C:/Users/colek/OneDrive/Desktop/ATP_Serve_Return_Stats/ATP_Return_4_7_22.html") as fp: #imports returning data, hmtl file downloaded from ATP website
     soupReturn = BeautifulSoup(fp, 'html.parser')
 
-trReturn = soupReturn.findAll('tr', class_ = "stats-listing-row")
+trReturn = soupReturn.findAll('tr', class_ = "stats-listing-row") #finds table in html
 
-return_data = [[td.getText() for td in trReturn[i].findAll(['th','td'])]
+return_data = [[td.getText() for td in trReturn[i].findAll(['th','td'])] #goes through each row to extract data from table
         for i in range(len(trReturn))
         ]
 
-dfReturn = pd.DataFrame(return_data)
-dfReturn = dfReturn.drop(0,axis=1)
+dfReturn = pd.DataFrame(return_data) #converts return statistics into dataframe
+dfReturn = dfReturn.drop(0,axis=1) #drops unneccessary column
 
-dfReturn.columns = ['Player', 'Return_Rating','1st_Serve_Return_Pct', '2nd_Serve_Return_Pct','Return_Games_Won_Pct','Break_Points_Won_Pct']
+dfReturn.columns = ['Player', 'Return_Rating','1st_Serve_Return_Pct', '2nd_Serve_Return_Pct','Return_Games_Won_Pct','Break_Points_Won_Pct'] #creates header titles for return stats
 
 
 firstServeReturnPct = dfReturn['1st_Serve_Return_Pct'].astype(str)
 firstServeReturnPct = firstServeReturnPct.tolist()
-firstServeReturnPct = [i.replace('%','') for i in firstServeReturnPct]
+firstServeReturnPct = [i.replace('%','') for i in firstServeReturnPct] #removes % sign from data in column so data can be used as numbers (not as string)
 
 secondServeReturnPct = dfReturn['2nd_Serve_Return_Pct'].astype(str)
 secondServeReturnPct = secondServeReturnPct.tolist()
-secondServeReturnPct = [i.replace('%','') for i in secondServeReturnPct]
+secondServeReturnPct = [i.replace('%','') for i in secondServeReturnPct] #removes % sign from data
 
 
 dfReturn['1st_Serve_Return_Pct'] = firstServeReturnPct
 dfReturn['2nd_Serve_Return_Pct'] = secondServeReturnPct
 
-dfReturn['1st_Serve_Return_Pct'] = pd.to_numeric(dfReturn['1st_Serve_Return_Pct'])
-dfReturn['2nd_Serve_Return_Pct'] = pd.to_numeric(dfReturn['2nd_Serve_Return_Pct'])
+dfReturn['1st_Serve_Return_Pct'] = pd.to_numeric(dfReturn['1st_Serve_Return_Pct']) #converts datatype to numeric
+dfReturn['2nd_Serve_Return_Pct'] = pd.to_numeric(dfReturn['2nd_Serve_Return_Pct'])#converts datatype to numeric
 
 
-#.651(70-19.8)+.349(53.1-39.7) + 50 to find their chances of winning on serve, this is a great formula. takes into account how often they make the first serve
-
+#this method used the collected returning and serving stats for each player to determine two player's probability of winning a point on their serve in the given match. 
+#the method takes in two strings of each players name in the match
 def calculateServePct(playerName1, playerName2):
     player1Serve = data.loc[data['Player'] == playerName1]
     player2Serve = data.loc[data['Player'] == playerName2]
@@ -97,30 +96,16 @@ def calculateServePct(playerName1, playerName2):
     player2FirstServeReturnPct = player2Return['1st_Serve_Return_Pct']
     player1SecondServeReturnPct = player1Return['2nd_Serve_Return_Pct']
     player2SecondServeReturnPct = player2Return['2nd_Serve_Return_Pct']
-    #p1 = player1FirstServeProp.values
-    p1 = player1FirstServeProp.values*(player1FirstServeWon.values-(player2FirstServeReturnPct.values)) + player1SecondServeProp.values*(player1SecondServeWon.values-(player2SecondServeReturnPct.values)) + 40
+    #formula for determining serving percentages below, takes into account various stats such as % first serves made, % first serves won when made, % second serve points won in addition to returning statistics
+    #40 is used as a baseline and then added to according to each player's serving and returning statistics
+    #it is probably important to consider double faulting percentages in order to make data after the first serve is missed more accurate. This is somewhat included in the % won on second serve stat but not completely. Return stats for second serve are irrelevant if there is a high double fault percentage, so an improved formula should account for this
+    p1 = player1FirstServeProp.values*(player1FirstServeWon.values-(player2FirstServeReturnPct.values)) + player1SecondServeProp.values*(player1SecondServeWon.values-(player2SecondServeReturnPct.values)) + 40 
     p2 = player2FirstServeProp.values*(player2FirstServeWon.values-player1FirstServeReturnPct.values) + player2SecondServeProp.values*(player2SecondServeWon.values-player1SecondServeReturnPct.values) + 40
 
     #print(playerName1, p1) 
     #print(playerName2, p2)
     return(p1,p2)
-    #print(player1SecondServeProp)
 
 #calculateServePct("Jaume Munar", "Tallon Griekspoor") 
 
 
-
-#eloDF = pd.read_csv("C:/Users/colek/OneDrive/Desktop/Tennis_Prediction_Model/Elo_Ratings.csv")
-
-#eloDF = eloDF.dropna(axis=1)
-#eloDF = eloDF[['Player','Elo']]
-#df = pd.merge(dfReturn, data, on='Player')
-
-#eloDF['Player'] = eloDF['Player'].astype(str)
-#eloDF['Elo'] = eloDF['Elo'].astype(int)
-#df['Player'] = df['Player'].astype(str)
-
-#df = pd.merge(df, eloDF, on='Player')
-
-#print(df)
-#print(eloDF)
